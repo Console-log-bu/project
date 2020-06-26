acca82df12d85b3c88a25298013156279a3c1a81
Vitya <vit9popov@gmai.com>
2020-06-26 21:01:44 +0300

    second version

--- films.js
+++ films.js
@@ -1,4 +1,9 @@
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
+
+/* while (numberOfFilms === '' || numberOfFilms.length >= 50 ) {
+   numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
+}
+ */
 const personalMovieDB = {
    count: numberOfFilms,
    movies: {},
@@ -7,12 +12,31 @@ const personalMovieDB = {
    privat: false
 };
 
-let lastFilm = prompt('Один из последних просмотренных фильмов?','');
-let evaluationFilm = prompt('На сколько оцените его?','');
-let a = prompt('Один из последних просмотренных фильмов?','');
-let b = prompt('На сколько оцените его?','');
 
-personalMovieDB.movies[lastFilm] = evaluationFilm;
-personalMovieDB.movies[a] = b;
+if (personalMovieDB.count < 10) {
+   alert('Просмотренно довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+   alert('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+   alert('Вы киноман');
+} else {
+   console.log('Произошла ошыбка');
+}
+
+
+for (let i = 0; i < 2; i++) {
+   let lastFilm = prompt('Один из последних просмотренных фильмов?');
+   let evaluationFilm = prompt('На сколько оцените его?');
+
+
+   if (lastFilm != null && evaluationFilm != null && lastFilm != '' && evaluationFilm != '' && lastFilm.length < 50) {
+      personalMovieDB.movies[lastFilm] = evaluationFilm;
+      console.log('done');
+   } else {
+      i--;
+      console.log('error');
+   }
+ 
+}
 
 console.log(personalMovieDB);
